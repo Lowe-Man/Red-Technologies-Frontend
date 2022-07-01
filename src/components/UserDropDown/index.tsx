0064389ff9fd292249315903ad709fec9717e660
Alex Lowe <loweman64@outlook.com>
2022-06-30 22:48:09 -0400

    feat: Create Table

---
+++ src/components/UserDropDown/index.tsx
@@ -0,0 +1,9 @@
+export default function UserDropDown() {
+    return (
+        <ul className="user-dropdown">
+            <li className="user-dropdown_item">Login</li>
+            <li className="user-dropdown_item">Signup</li>
+            <li className="user-dropdown_item">Logout</li>
+        </ul>
+    )
+}
---
+++ src/views/Home/index.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+import Page from "../../components/Page";
+import BasicTable from "../../components/Table";
+
+export default function Home(){
+    return <Page headerTitle={"Home"}>
+        <BasicTable></BasicTable>
+    </Page>
+}
