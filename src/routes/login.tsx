c85a88d4a686551fcc5f7addf9486b54b1ec9726
Alex Lowe <loweman64@outlook.com>
2022-07-01 22:10:32 -0400

    feat: Signup User with Proper Response

--- src/routes/login.tsx
+++ src/routes/login.tsx
@@ -1,10 +1,24 @@
 import Page from "../components/Page";
+import {MDBBtn, MDBCol, MDBContainer, MDBInput, MDBRow} from "mdb-react-ui-kit";
 
 export default function Login() {
     return (<Page headerTitle={"Login"}>
-            <main style={{padding: "1rem 0"}}>
-                <h2>Login Page</h2>
-            </main>
+            <form>
+                <p className="h5 text-center mb-4">Login</p>
+                <div className="grey-text">
+                    <MDBInput
+                        label="email"
+                        type="email"
+                    />
+                    <MDBInput
+                        label="password"
+                        type="password"
+                    />
+                </div>
+                <div className="text-center">
+                    <MDBBtn>Login</MDBBtn>
+                </div>
+            </form>
         </Page>
     );
 }
--- src/routes/signup.tsx
+++ src/routes/signup.tsx
@@ -1,10 +1,47 @@
 import Page from "../components/Page";
+import {MDBBtn, MDBCol, MDBContainer, MDBInput, MDBRow} from "mdb-react-ui-kit";
+import React, {FormEvent, useRef} from "react";
 
 export default function Signup() {
+    const [name, setName] = React.useState("");
+    const [email, setEmail] = React.useState("");
+    const [password, setPassword] = React.useState("");
+
+    const submitForm = (event: FormEvent<HTMLFormElement>) => {
+        event.preventDefault();
+        console.log(name, email, password);
+
+    }
+
     return (<Page headerTitle={"Signup"}>
-            <main style={{padding: "1rem 0"}}>
-                <h2>Signup Page</h2>
-            </main>
+            <form onSubmit={submitForm}>
+                <p className="h5 text-center mb-4">Sign up</p>
+                <div className="grey-text">
+                    <MDBInput
+                        label="full name"
+                        type="text"
+                        value={name}
+                        onChange={(event) => setName(event.target.value)}
+                    />
+                    <br/>
+                    <MDBInput
+                        label="email"
+                        type="email"
+                        value={email}
+                        onChange={(event) => setEmail(event.target.value)}
+                    />
+                    <br/>
+                    <MDBInput
+                        label="password"
+                        type="password"
+                        value={password}
+                        onChange={(event) => setPassword(event.target.value)}
+                    />
+                </div>
+                <div className="text-center">
+                    <MDBBtn>Signup</MDBBtn>
+                </div>
+            </form>
         </Page>
     );
 }
